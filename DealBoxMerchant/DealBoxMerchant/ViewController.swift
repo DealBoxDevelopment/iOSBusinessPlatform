b511a1362abe928c718337fe5f580c5bf7f4939c
Sky Xu <skyrocks88xmz@gmail.com>
2018-07-09 15:29:56 -0700

    finish first two cell

--- DealBoxMerchant/DealBoxMerchant/ViewController.swift
+++ DealBoxMerchant/DealBoxMerchant/ViewController.swift
@@ -18,6 +18,25 @@ class ViewController: UIViewController {
         
         let nibCellPrice = UINib(nibName: "PriceCell", bundle: Bundle.main)
         tableView.register(nibCellPrice, forCellReuseIdentifier: "priceCell")
+        
+        let nibImgCell = UINib(nibName: "DealImgCell", bundle: Bundle.main)
+        tableView.register(nibImgCell, forCellReuseIdentifier: "dealImgCell")
+        
+        let nibSummaryCell = UINib(nibName: "DealSummaryCell", bundle: Bundle.main)
+        tableView.register(nibSummaryCell, forCellReuseIdentifier: "dealSummaryCell")
+        
+        let nibPreconditionCell = UINib(nibName: "DealPreconditionCell", bundle: Bundle.main)
+        tableView.register(nibPreconditionCell, forCellReuseIdentifier: "dealPreconditionCell")
+        
+        let nibLocationCell = UINib(nibName: "DealLocationCell", bundle: Bundle.main)
+        tableView.register(nibLocationCell, forCellReuseIdentifier: "dealLocationCell")
+        
+        let nibLaunchDateCell = UINib(nibName: "LaunchDateCell", bundle: Bundle.main)
+        tableView.register(nibLaunchDateCell, forCellReuseIdentifier: "launchDateCell")
+        
+        let nibAgreementCell = UINib(nibName: "SignAgreementCell", bundle: Bundle.main)
+        tableView.register(nibAgreementCell, forCellReuseIdentifier: "signAgreementCell")
+        
     }
 }
 
@@ -33,22 +52,22 @@ extension ViewController: UITableViewDataSource {
             guard let cell = tableView.dequeueReusableCell(withIdentifier: "priceCell") as? PriceCell else { fatalError("pricecell configure failed") }
             return cell
         case 1:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "dealImgCell") as? DealImgCell else { fatalError("deal cell failed") }
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "dealImgCell") as? DealImgCell else { fatalError("deal img cell failed") }
             return cell
         case 2:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "dealImgCell") as? DealImgCell else { fatalError("deal cell failed") }
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "dealSummaryCell") as? DealSummaryCell else { fatalError("deal summary cell failed") }
             return cell
         case 3:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "dealImgCell") as? DealImgCell else { fatalError("deal cell failed") }
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "dealPreconditionCell") as? DealPreconditionCell else { fatalError("deal precondition cell failed") }
             return cell
         case 4:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "dealImgCell") as? DealImgCell else { fatalError("deal cell failed") }
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "dealLocationCell") as? DealLocationCell else { fatalError("deal cell failed") }
             return cell
         case 5:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "dealImgCell") as? DealImgCell else { fatalError("deal cell failed") }
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "launchDateCell") as? LaunchDateCell else { fatalError("launch cell failed") }
             return cell
         case 6:
-            guard let cell = tableView.dequeueReusableCell(withIdentifier: "dealImgCell") as? DealImgCell else { fatalError("deal cell failed") }
+            guard let cell = tableView.dequeueReusableCell(withIdentifier: "signAgreementCell") as? SignAgreementCell else { fatalError("sign agreement cell failed") }
             return cell
         default:
             return PriceCell()
